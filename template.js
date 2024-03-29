84e96c35efc4a156a36ed73973e2911d7e3ddb13
junayedhasanLearning <junayedhasan0271@gmail.com>
2024-03-29 15:56:45 +0600

    Template string , multiline string

---
+++ template.js
@@ -0,0 +1,13 @@
+const firstName = "Junayed Hasan";
+const lastName = "Sakib";
+
+const fullName = `${firstName} ${lastName} is a smart boy!`;
+// console.log(fullName);
+
+const multiline = "Hello There, I am here \n" + "I am Sakib";
+const multiline1 = `Hello There, I am here
+                    my name is sakib
+                    i am a programmer and
+                    I am learning`;
+                    
+console.log(multiline1);
