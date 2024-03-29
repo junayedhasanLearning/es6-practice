4c683a570df51c215b42007e616bcb7bf4d2d48a
junayedhasanLearning <junayedhasan0271@gmail.com>
2024-03-29 19:28:42 +0600

    full cleared concept about destructure

--- destructure.js
+++ destructure.js
@@ -1,15 +1,28 @@
 const profile = {
-    Name : 'Tom Holland',
-    Age : 27, 
-    gfName : 'Zendaya',
-    job : 'Spider-man'
+    Name: 'Tom Holland',
+    Age: 27,
+    gfName: 'Zendaya',
+    job: 'Spider-man'
 };
 const profile1 = {
-    gfName : 'Error',
-    job : 'iron Man'
+    gfName: 'Error',
+    job: 'iron Man'
 };
 
 // const {gfName, job} = profile;
-const {gfName, job} = profile1;
+const {
+    gfName,
+    job
+} = profile1;
 
-console.log(gfName);
+
+
+const friends = [
+    'Shah Rukh Khan', 'Salman Khan', 'Amir khan', 'Saif Ali Khan', 'Hrittik Roshan', 'Ranbeer Kapoor'
+];
+
+const [chotoFriend, chotoFriend2, ...remainingFriends] = friends;
+console.log(typeof chotoFriend);
+console.log(typeof chotoFriend2);
+console.log(typeof remainingFriends);
+console.log(chotoFriend, chotoFriend2, remainingFriends);
