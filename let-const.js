23d3e227ce2e0af13af02f94acd1da5da896d18c
junayedhasanLearning <junayedhasan0271@gmail.com>
2024-03-29 15:28:23 +0600

    Let & const concept fully cleared

---
+++ let-const.js
@@ -0,0 +1,21 @@
+const number = [12,32,45,65,64];
+number.unshift(56)
+
+// console.log(number.length);
+
+
+const profile = {
+    Name : 'Junayed Hasan Saikib',
+    Age : 12
+}
+
+// console.log(profile);
+
+let age = 12;
+age =  13;
+console.log(age);
+
+let sum = 0;
+for(let i = 0; i <= 10; i++){
+    sum = sum+i;
+};
